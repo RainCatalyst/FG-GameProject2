7ce916a7abebc04add6f477efa738e830b5ffd70
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2023-01-31 12:52:04 +0100

    Added joystick support (not tested)

--- Assets/Modules/Player/CharacterLogic.cs
+++ Assets/Modules/Player/CharacterLogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterLogic : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class CharacterLogic : MonoBehaviour
     private int playerNumber = 1;
 
     private string horizontalAxis;
+    private string horizontalAxisJoy;
     private string verticalAxis;
+    private string verticalAxisJoy;
 
     private void Awake()
     {
         horizontalAxis = "Horizontal" + playerNumber;
+        horizontalAxisJoy = "HorizontalJoy" + playerNumber;
         verticalAxis = "Vertical" + playerNumber;
+        verticalAxisJoy = "VerticalJoy" + playerNumber;
 
         characterMovement = GetComponent<CharacterMovement>();
     }
@@ -22,10 +27,14 @@ public class CharacterLogic : MonoBehaviour
     private void Update()
     {
         Vector2 moveDirection;
-        moveDirection.x = Input.GetAxis(horizontalAxis);
-        moveDirection.y = Input.GetAxis(verticalAxis);
-
+        moveDirection.x = Input.GetAxis(horizontalAxis) + Input.GetAxis(horizontalAxisJoy);
+        moveDirection.y = Input.GetAxis(verticalAxis) + Input.GetAxis(verticalAxisJoy);
+        
         characterMovement.Move(moveDirection);
     }
 
+    // public void OnMove(InputAction.CallbackContext ctx)
+    // {
+    //     characterMovement.Move(ctx.ReadValue<Vector2>());
+    // }
 }
