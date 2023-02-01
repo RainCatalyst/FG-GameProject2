8d21136dcb234e61b8ff5c6dcd356ef5cc6ed70b
alicekamil <alice.anastasiak@outlook.com>
2023-02-01 13:21:22 +0100

    Scriptable object

--- Assets/Modules/Items/Scripts/ItemData.cs
+++ Assets/Modules/Items/Scripts/ItemData.cs
@@ -5,20 +5,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "NewItem", menuName = "Data")]
+[CreateAssetMenu(fileName = "NewItem", menuName = "Data/ItemData")]
     public class ItemData : ScriptableObject
     {
         public string Id => _id;
         public GameObject GameObject => gameObject;
-        public bool IsItem => _isItem;
-        public string GrindedId => String.IsNullOrEmpty(_grindedId) ? null : _grindedId; //?
 
         [SerializeField]
         private string _id;
         [SerializeField]
         public GameObject gameObject;
-        [SerializeField]
-        private bool _isItem;
-        [SerializeField]
-        private string _grindedId;
+
+    public void Print()
+    {
+        Debug.Log(_id);
+    }
     }
