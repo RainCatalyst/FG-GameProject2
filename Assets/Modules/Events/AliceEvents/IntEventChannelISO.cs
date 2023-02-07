f1c01f1dbb263b3b7d52202bf1989563b848dc30
alicekamil <alice.anastasiak@outlook.com>
2023-02-07 12:07:54 +0100

    Added comments in intevent

--- Assets/Modules/Events/AliceEvents/IntEventChannelISO.cs
+++ Assets/Modules/Events/AliceEvents/IntEventChannelISO.cs
@@ -4,9 +4,9 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "NewIntEventChannel", menuName = "Events/Int Event Channel")]
 public class IntEventChannelSO : ScriptableObject
 {
-    public event UnityAction<int> OnValueUpdated;
-    public event UnityAction OnValueAdded;
+    public event UnityAction<int> OnValueUpdated; 
+    public event UnityAction OnValueAdded; //triggers when number increase
 
-    public void AddValue() => OnValueAdded?.Invoke();
+    public void AddValue() => OnValueAdded?.Invoke(); 
     public void UpdateValue(int value) => OnValueUpdated?.Invoke(value);
 }
