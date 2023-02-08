2761508f7986b34d42455ab388e4c157d311580d
alicekamil <alice.anastasiak@outlook.com>
2023-02-08 15:04:34 +0100

    Made super basic fire logic

--- Assets/FireSpawnManager.cs
+++ Assets/FireSpawnManager.cs
@@ -7,12 +7,14 @@ public class FireSpawnManager : MonoBehaviour
 
     private void Update()
     {
+        print($"active prefabs: {_activePrefabs.Count}");
         if (_activePrefabs.Count < _spawnLocation.Count)
         {
             _timer += Time.deltaTime;
-            if (_timer >= Random.Range(_minDelay, _maxDelay))
+            if (_timer >= _randomTimer)
             {
                 SpawnFirePrefab();
+                _randomTimer = Random.Range(_minDelay, _maxDelay);
                 _timer = 0f;
             }
         }
@@ -21,22 +23,24 @@ public class FireSpawnManager : MonoBehaviour
     {
         int randomIndex = Random.Range(0, _spawnLocation.Count);
         Transform spawnLocation = _spawnLocation[randomIndex];
-        Instantiate(_prefabToSpawn, spawnLocation.position, spawnLocation.rotation);
-        _activePrefabs.Add(_prefabToSpawn);
+        DestroyFire newPrefab = Instantiate(_prefabToSpawn, spawnLocation.position, spawnLocation.rotation);
+        _activePrefabs.Add(newPrefab);
+        newPrefab.Destroyed += OnDestroyedPrefab;
     }
-    public void OnDestroyedPrefab(GameObject prefab)
+    public void OnDestroyedPrefab(DestroyFire prefab)
     {
         _activePrefabs.Remove(prefab);
+        prefab.Destroyed -= OnDestroyedPrefab;
     }
 
     [SerializeField]
     private List<Transform> _spawnLocation;
-    private List<GameObject> _activePrefabs = new List<GameObject>();
+    private List<DestroyFire> _activePrefabs = new List<DestroyFire>();
 
     [SerializeField]
-    private GameObject _prefabToSpawn;
+    private DestroyFire _prefabToSpawn;
 
-
+    private float _randomTimer = 5f;
     [Header("Timer for fire prefab")]
     [SerializeField]
     private float _minDelay = 1f;
