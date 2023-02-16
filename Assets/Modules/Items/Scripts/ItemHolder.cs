cf84b72a95529d103621908ed30f3d6d6ea547f1
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2023-02-16 16:33:47 +0100

    Added animation to item holder

--- Assets/Modules/Items/Scripts/ItemHolder.cs
+++ Assets/Modules/Items/Scripts/ItemHolder.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 //Pickup logic, this component displays an item that the player is holding
@@ -6,6 +7,11 @@ public class ItemHolder : MonoBehaviour
     public string ItemId => _id;
     public ItemData Item => ItemDatabase.Get(_id);
     public Transform ItemParent => _itemParent;
+
+    private void Awake()
+    {
+        _defaultScale = _itemParent.localScale;
+    }
 
     public void SetItem(string id)
     {
@@ -19,11 +25,15 @@ public class ItemHolder : MonoBehaviour
         if (_id != null)
         {
             _activeItem = Instantiate(Item.GameObject, _itemParent);
+            LeanTween.cancel(_itemParent.gameObject);
+            _itemParent.localScale = Vector3.zero;
+            LeanTween.scale(_itemParent.gameObject, _defaultScale, 0.21f).setEaseOutBack();
         }
     }
 
     [SerializeField]
     private Transform _itemParent;
     private GameObject _activeItem;
+    private Vector3 _defaultScale;
     private string _id;
 }
