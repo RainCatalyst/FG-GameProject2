f8ab094129e25836e08a2c2f74e656564e8a15da
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2023-02-17 15:47:02 +0100

    Update RecipeHint.cs

--- Assets/Modules/Interactions/Scripts/RecipeHint.cs
+++ Assets/Modules/Interactions/Scripts/RecipeHint.cs
@@ -6,18 +6,19 @@ public class RecipeHint : MonoBehaviour
     public void SetRecipeSprite(Sprite sprite, int idx)
     {
         Recipes[idx].sprite = sprite;
+        Recipes[idx].gameObject.SetActive(sprite != null);
 
-        for (int i = 0; i < 2; i++)
-        {
-            Recipes[idx].gameObject.SetActive(Recipes[i].sprite != null);
-            
-        }
-        
-        for (int i = 0; i < 2; i++)
-        {
-            if (Recipes[1 - idx].gameObject.activeSelf && Recipes[1 - idx].sprite == Recipes[idx].sprite)
-                Recipes[idx].gameObject.SetActive(false);
-        }
+        // for (int i = 0; i < 2; i++)
+        // {
+        //     Recipes[idx].gameObject.SetActive(Recipes[i].sprite != null);
+        //     
+        // }
+        //
+        // for (int i = 0; i < 2; i++)
+        // {
+        //     if (Recipes[1 - idx].gameObject.activeSelf && Recipes[1 - idx].sprite == Recipes[idx].sprite)
+        //         Recipes[idx].gameObject.SetActive(false);
+        // }
     }
     
     public Image[] Recipes;
