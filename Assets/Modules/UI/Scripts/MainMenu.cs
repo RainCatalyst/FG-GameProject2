f0c1b1062965fec32d17c481be7f865c54a33e83
Seth Marrison <sethers932@gmail.com>
2023-02-24 15:37:49 +0100

    encounters balanced + main menu bug fix

--- Assets/Modules/UI/Scripts/MainMenu.cs
+++ Assets/Modules/UI/Scripts/MainMenu.cs
@@ -14,7 +14,7 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void Update()
