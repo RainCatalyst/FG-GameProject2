46ad0b20a5b0acb63cfe6c582ee67cf8cac4ff9a
Seth Marrison <sethers932@gmail.com>
2023-03-05 15:49:04 +0100

    credits animation

--- Assets/Modules/UI/Scripts/MainMenu.cs
+++ Assets/Modules/UI/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -17,4 +18,45 @@ public class MainMenu : MonoBehaviour
         LeanTween.cancelAll();
         SceneManager.LoadScene("Game");
     }
+    
+    public void CreditsClicked()
+    {
+        _isClicked = true;
+    }
+    
+    public void CreditsBack()
+    {
+        _isClicked = false;
+    }
+
+    private void Update()
+    {
+        if (_isClicked)
+        {
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, _camTarget.transform.position, _camSpeed * Time.deltaTime);
+            _camera.transform.rotation = Quaternion.Lerp(_camera.transform.rotation,_camTarget.transform.rotation, _camRotateSpeed * Time.deltaTime);
+            _camera.transform.SetParent(_camTarget.transform);
+        }
+        else
+        {
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, _camOrigin.transform.position, _camSpeed / 2 * Time.deltaTime);
+            _camera.transform.rotation = Quaternion.Lerp(_camera.transform.rotation,_camOrigin.transform.rotation, _camRotateSpeed / 2 * Time.deltaTime);
+            _camera.transform.SetParent(null);
+        }
+        
+    }
+
+
+    [SerializeField] 
+    private GameObject _camera;
+    [SerializeField] 
+    private GameObject _camTarget;
+    [SerializeField] 
+    private GameObject _camOrigin;
+    [SerializeField] 
+    private float _camSpeed = 15f;
+    [SerializeField] 
+    private float _camRotateSpeed = 5f;
+
+    private bool _isClicked;
 }
